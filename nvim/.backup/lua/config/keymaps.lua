8fc8d1759f435ed444f051a07844bcb02d74b23d
diego <diegoa2992@gmail.com>
2024-10-04 12:44:40 -0500

    Update dotfiles: 2024-10-04 12:44:40

---
+++ nvim/.backup/lua/config/keymaps.lua
@@ -0,0 +1,11 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+-- telescope plugins
+local builtin = require("telescope.builtin")
+vim.keymap.set("n", "<C-p>", builtin.find_files, {})
+vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+vim.keymap.set("n", "<leader>fb", builtin.buffers, {})
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
