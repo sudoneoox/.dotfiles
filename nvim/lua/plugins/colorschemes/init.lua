ef1948cb04e1c8b75575521be074a749a8e55fa7
diego <diegoa2992@gmail.com>
2024-10-11 00:14:38 -0500

    Update dotfiles: 2024-10-11 00:14:38

--- nvim/lua/plugins/colorschemes/init.lua
+++ nvim/lua/plugins/colorschemes/init.lua
@@ -2,10 +2,11 @@ local dir = 'plugins.colorschemes.'
 
 return {
   -- require(dir .. 'tokyodark'),
-  require(dir .. 'catppuccin'),
+  -- require(dir .. 'catppuccin'),
   -- require(dir .. 'lackluster'),
   -- require(dir .. 'everforest'),
   -- require(dir .. 'rose-pine'),
   -- require(dir .. 'aurora'),
   -- require(dir .. 'kanagawa'),
+  require(dir .. 'github-nvim'),
 }
