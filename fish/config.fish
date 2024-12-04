d3d9405b090a2fc85867c9541c08754f1ef52484
X0@laptop <diegoa2992@gmail.com>
2024-12-04 02:11:15 -0600

    Update dotfiles: 2024-12-04 02:11:15

--- fish/config.fish
+++ fish/config.fish
@@ -4,5 +4,6 @@ set -q XDG_CONFIG_HOME || set XDG_CONFIG_HOME "$HOME/.config"
 source $XDG_CONFIG_HOME/fish/aliases.fish
 source $XDG_CONFIG_HOME/fish/developer.fish
 source $XDG_CONFIG_HOME/fish/main.fish
+source $XDG_CONFIG_HOME/fish/temp_dev_vars.fish
 #source $XDG_CONFIG_HOME/fish/prompt.fish
 
---
+++ fish/temp_dev_vars.fish
@@ -0,0 +1,8 @@
+# -------------------------- COUGAR LOUNGE ------------------------------------
+set -x JWT_SECRET 'UNZDmm25rIde/No79mZjqVmQbyQpZOda0I0sXOE74Y2rIbPwWswBsJAToEVLoz2L
+6LpEIjPiKPbB2mPiZ5P/QgqjyvRPexv35kdc5AdJKEBUp/ksrTjr25w5Xq9jkE
+ZqevmYBYYRNZZmcER1GtpmNlLztQmfujvSdJKUkZZFSYcqkV2RFDo69SR5vNqlXD
+tmrXWeqmj0VxxFfjZ4FWWFiO1o8L/t1jGIys4g=='
+set -x SUPABASE_API_KEY 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqcHNlYWphcnB0a2VpdGhhcHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzI5OTc5OTksImV4cCI6MjA0ODU3Mzk5OX0.lq-d-K7Mx9tBF-SWFgDbLJn5Uv20EWxIu1rtbjdJ0eI'
+set -x SUPABASE_PROJECT_URL 'https://djpseajarptkeithapup.supabase.co'
+# -----------------------------------------------------------------------------
