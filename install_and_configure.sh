846b85fb4873448b95e96fc0f0f4cd70ca0ed485
X0@Arch Desktop Diego <diegoa2992@gmail.com>
2025-02-01 16:07:20 -0600

    improvement to install packages script

--- install_and_configure.sh
+++ install_and_configure.sh
@@ -1,91 +1,76 @@
 #!/bin/bash
-
 # Set colors for output
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 YELLOW='\033[1;33m'
 NC='\033[0m' # No Color
 
-# Define package lists
-DEPENDENCY_PACKAGES=(
-  "xorg" "xorg-xinit" "xterm" "git" "polkit" "polkit-gnome" "alsa-utils"
+# Package groups
+TIME_SYNC=("ntp" "chrony")
+
+UTILS=("xorg" "xorg-xinit" "xterm" "git" "polkit" "polkit-gnome" "alsa-utils"
   "pavucontrol" "pipewire" "pipewire-pulse" "pipewire-alsa"
-  "arandr" "at" "acpi" "git" "brightnessctl"
-)
+  "arandr" "at" "acpi" "git" "brightnessctl" "rsync" "node" "network-manager-applet" "volctl" "playerctl" "awesome-luajit-git" "flameshot" 
+  "clipmenu-rofi" "blueman" "maim" "i3lock-color" "nitrogen" "rofimoji" "cups" "btop" "bat" "fzf" "tldr" "wget")
 
-AUR_PACKAGES=(
-  "network-manager-applet" "volctl" "rofi" "lxappearance" "qt5ct"
-  "kvantum" "pop-theme" "papirus-icon-theme" "awesome-luajit-git" "playerctl"
-  "flameshot" "clipmenu-rofi" "blueman" "rofi" "clipmenu-rofi" "maim" "i3lock-color"
-  "nitrogen" "localsend-bin" "rofimoji"
-)
+THEMES=("lxappearance" "qt5ct" "kvantum" "pop-theme" "papirus-icon-theme" "pop-theme")
 
-FONT_PACKAGES=(
-  "ttf-iosevka-nerd" "ttf-mononoki-nerd" "ttf-roboto" "ttf-fira-mono" "noto-fonts-emoji"
-)
-
-CORE_PACKAGES=(
-  "cups" "spotify" btop" "bat" "filelight" "neovim" "tldr" "zen-browser-bin" "discord" "fzf" "obsidian"
-)
+FONT_PACKAGES=("ttf-iosevka-nerd" "ttf-mononoki-nerd" "ttf-roboto" "ttf-fira-mono" "noto-fonts-emoji")
 
 NVIM_CONFIG=(
   "python-cpplint" "clang" "python-sqlfluff" "nodejs" "npm" "lua-language-server" "lazygit" "ruby" "neovim-tree-sitter-git" "lldb" "lazygit" "fd" "ripgrep"
 )
 
+APPLICATIONS=("localsend-bin" "rofi" "spotify" "filelight" "neovim" "zen-browser-bin" "discord" "obsidian")
+
+
+DEPENDENCY_PACKAGES=(
+  ${UTILS[*]} ${NVIM_CONFIG[*]} ${TIME_SYNC[*]}
+)
+
+ETC_PACKAGES=(
+  ${APPLICATIONS[*]} ${THEMES[*]} ${FONT_PACKAGES[*]}
+)
+
 # Function to display the main menu
 display_main_menu() {
-  echo -e "${YELLOW}Select an action:${NC}"
-  echo "1) Install dependency packages"
-  echo "2) Install fonts"
-  echo "3) Install Arc icons"
-  echo "4) Install core packages"
-  echo "5) Install all"
-  echo "6) Exit"
+  echo -e "${YELLOW}Install Packages?:${NC}"
 }
 
 # Function to get user selection
 get_selection() {
   local selection
-  read -p "Enter your choice: " selection
+  read -p "Enter your choice (y/n): " selection
   echo $selection
 }
 
+# Function to setup time synchronization
+setup_time_sync() {
+  echo -e "${GREEN}Setting up time synchronization...${NC}"
+  # Enable and start NTP service
+  sudo timedatectl set-ntp true
+  sudo systemctl enable --now chronyd
+  # Wait for initial sync
+  sleep 2
+  # Show sync status
+  timedatectl status
+}
+
 # Function to install core packages
-install_dependency_packages() {
+install_packages() {
   echo -e "${GREEN}Installing core packages...${NC}"
-  sudo pacman -S --needed "${DEPENDENCY_PACKAGES[@]}"
-  yay -S --needed "${DEPENDENCY_PACKAGES[@]}"
-  yay -S --needed "${NVIM_CONFIG[@]}"
+  yay -S --needed ${DEPENDENCY_PACKAGES[*]}
+  yay -S --needed ${ETC_PACKAGES[*]}
   sudo npm i -g eslint vim-language-server tree-sitter-cli neovim
-}
-
-# Function to install AUR packages
-install_aur_packages() {
-  echo -e "${GREEN}Installing AUR packages with yay...${NC}"
-  yay -S --needed "${AUR_PACKAGES[@]}"
-}
-
-# Function to install fonts
-install_fonts() {
-  echo -e "${GREEN}Installing fonts...${NC}"
-  yay -S --needed "${FONT_PACKAGES[@]}"
-}
-
-# Function to install Arc icons
-install_arc_icons() {
+  
+  # Setup time synchronization
+  setup_time_sync
+  
   echo -e "${GREEN}Installing Arc icons...${NC}"
   git clone https://github.com/horst3180/arc-icon-theme --depth 1 && cd arc-icon-theme
   ./autogen.sh --prefix=/usr
   sudo make install
   cd .. && rm -rf arc-icon-theme
-}
-
-# Function to install extra packages
-install_core_packages() {
-  echo -e "${GREEN}Installing extra packages...${NC}"
-  sudo pacman -S --needed "${CORE_PACKAGES[@]}"
-  yay -S --needed "${CORE_PACKAGES[@]}"
-  sudo pacman -S --needed wget
   wget micro.mamba.pm/install.sh && chmod +x install.sh && ./install.sh && rm install.sh
 }
 
@@ -94,39 +79,19 @@ main() {
   while true; do
     display_main_menu
     selection=$(get_selection)
-
     case $selection in
-    1)
-      install_dependency_packages
-      install_aur_packages
-      ;;
-    2)
-      install_fonts
-      ;;
-    3)
-      install_arc_icons
-      ;;
-    4)
-      install_core_packages
-      install_aur_packages
-      ;;
-    5)
-      install_core_packages
-      install_aur_packages
-      install_fonts
-      install_arc_icons
-      install_core_packages
-      ;;
-    6)
-      echo "Exiting..."
-      exit 0
-      ;;
-    *)
-      echo -e "${RED}Invalid selection. Please try again.${NC}"
-      ;;
+      y)
+        install_packages
+        ;;
+      n)
+        echo "Exiting..."
+        exit 0
+        ;;
+      *)
+        echo -e "${RED}Invalid selection.${NC}"
+        ;;
     esac
   done
 }
 
-# Run the main function
 main
